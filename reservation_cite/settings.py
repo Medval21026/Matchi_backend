48ac6b7f2db19a817c6bcdde7ae4930edd9c1ac3
Medval21026 <21026@supnum.mr>
2026-02-08 19:16:28 +0100

    dern

--- reservation_cite/settings.py
+++ reservation_cite/settings.py
@@ -120,18 +120,19 @@ WSGI_APPLICATION = 'reservation_cite.wsgi.application'
 # Pour Railway, utilisez les variables d'environnement
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', 'mysql.connector.django'),  # Utiliser mysql.connector au lieu de django.db.backends.mysql
-        'NAME': os.getenv('MYSQLDATABASE', os.getenv('MYSQL_DATABASE', 'cite')),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('MYSQLDATABASE', 'cite'),
         'USER': os.getenv('MYSQLUSER', 'root'),
-        'PASSWORD': os.getenv('MYSQLPASSWORD', os.getenv('MYSQL_ROOT_PASSWORD', '')),
+        'PASSWORD': os.getenv('MYSQLPASSWORD', ''),
         'HOST': os.getenv('MYSQLHOST', 'localhost'),
         'PORT': os.getenv('MYSQLPORT', '3306'),
         'OPTIONS': {
-            'sql_mode': 'STRICT_TRANS_TABLES',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
         },
     }
 }
+
 
 # Si MYSQL_URL est fourni (format: mysql://user:password@host:port/database)
 # Ne l'utiliser QUE si on n'est pas en développement local
